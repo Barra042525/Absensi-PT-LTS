ad904c06a647a13b36a074a01c6cb31cdcc33fc5
Barra042525 <barhama4598@gmail.com>
2026-09-14 10:37:17 +0700

    Fix format angka koordinat lat dan lng

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ let userLng = null;
 let isLocationValid = false;
 
 // KONFIGURASI 2 TITIK LOKASI & RADIUS (DALAM METER)
-const MAX_RADIUS_METER = 100; // Jarak maksimal (misal: 100 meter)
+const MAX_RADIUS_METER = 200; // Toleransi 200 meter (bagus untuk posisi dalam gedung/Lantai 3)
 
-// Ganti dengan koordinat asli lokasi Anda
-const TITIK_LOKASI_1 = { lat: -6.123456, lng: 106.876543, nama: "Villa, Jl. Mutiara Gading No.2a-6.157882374358362, 106.996006453364 blok F2, Setia Asih, Kec. Tarumajaya, Kabupaten Bekasi, Jawa Barat 17215" };
-const TITIK_LOKASI_2 = { lat: -6.234567, lng: 106.987654, nama: "Warehouse Lintas Tata Samudra-6.15708724406157, 106.99707665901374" };
+// Koordinat Asli Lokasi PT LINTAS TATA SAMUDERA
+const TITIK_LOKASI_1 = { 
+    lat: -6.157882374358362, 
+    lng: 106.996006453364, 
+    nama: "Villa Mutiara Gading (Kantor Pusat)" 
+};
 
+const TITIK_LOKASI_2 = { 
+    lat: -6.15708724406157, 
+    lng: 106.99707665901374, 
+    nama: "Warehouse Lintas Tata Samudera" 
+};
 // Memulai Kamera Webcam
 function startCamera() {
     const video = document.getElementById('webcam');
